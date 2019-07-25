4374872985289399faaefe68a7d5c129eafae8f0
Mario Castro Morera <mariocastromorera@gmail.com>
2019-07-25 16:12:46 -0600

    Add build directory to be removed

--- homework1/PartII/clean.sh
+++ homework1/PartII/clean.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
-rm -rf aclocal.m4 autom4te.cache build-aux/* install-sh missing\
+rm -rf aclocal.m4 autom4te.cache build-aux install-sh missing\
       depcomp config.log config.status configure compile\
       lib/Makefile lib/Makefile.in Makefile \
       Makefile.in src/Makefile src/Makefile.in \
       lib/libmemcheck src/memcheck src/memcheck.o \
       lib/libmemcheck.o lib/.deps*\
-      output
+      output build
