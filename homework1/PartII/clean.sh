81aab091b9cd47c01007d629940b42f5ac92efcb
Mario Castro Morera <mariocastromorera@gmail.com>
2019-07-25 16:50:09 -0600

    Add libmemcheck.so to be removed

--- homework1/PartII/clean.sh
+++ homework1/PartII/clean.sh
@@ -4,6 +4,6 @@ rm -rf aclocal.m4 autom4te.cache build-aux install-sh missing\
       depcomp config.log config.status configure compile\
       lib/Makefile lib/Makefile.in Makefile \
       Makefile.in src/Makefile src/Makefile.in \
-      lib/libmemcheck src/memcheck src/memcheck.o \
+      lib/libmemcheck.so src/memcheck src/memcheck.o \
       lib/libmemcheck.o src/.deps* lib/.deps* \
       output build
