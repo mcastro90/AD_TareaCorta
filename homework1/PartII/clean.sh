8d51453fd4f6db1808c3583413164251012bf54c
Mario Castro Morera <mariocastromorera@gmail.com>
2019-07-25 22:44:33 -0600

    Update clean.sh script

--- homework1/PartII/clean.sh
+++ homework1/PartII/clean.sh
@@ -1,9 +1,11 @@
 #!/bin/sh
 
+make clean
+
 rm -rf aclocal.m4 autom4te.cache build-aux install-sh missing\
       depcomp config.log config.status configure compile\
       lib/Makefile lib/Makefile.in Makefile \
       Makefile.in src/Makefile src/Makefile.in \
       lib/libmemcheck.so src/memcheck src/memcheck.o \
       lib/libmemcheck.o src/.deps* lib/.deps* \
-      output build
+      output build config.* libtool ltmain.sh m4
